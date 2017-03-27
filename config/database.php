8e723d33122af281e74848a74ab4b52f8e81a230
quanghoan <quanghoan1992@gmail.com>
2017-03-27 23:47:06 +0700

    fix

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
+            // 'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'quickstart_basic'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', '123456'),
